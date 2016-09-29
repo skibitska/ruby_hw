3eea9e95e1cca354eff3be9080aede9544f4120d
Lida Skibitska <lskibitska@gmail.com>
2016-09-29 12:55:01 +0300

    Fixed recursion

--- hw1/reverse.rb
+++ hw1/reverse.rb
@@ -1,8 +1,8 @@
-def rec(array, index = array.length)
-	return if (index == -1)
-	print(array[index])
-	index = index - 1
-	rec(array,index)
+def rec(a, b, i = a.length)
+  return print(b) if (i == 0)
+  i = i - 1
+  b.push(a[i])
+  rec(a, b, i)
+  end
 	
-end
-	
+rec([1,2,3,4], []) => [4,3,2,1]
