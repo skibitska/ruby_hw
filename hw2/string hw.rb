4d3fe08eb89d0e1e44e5dde54cea2a90be771d31
skibiska <lskibitska@gmail.com>
2016-10-05 00:03:48 +0300

    fixed hw2 again and again

--- hw2/string hw.rb
+++ hw2/string hw.rb
@@ -7,12 +7,10 @@ index = 1
 for i in (0..s_length-2) do
   if string[i] == string[i+1]
     index += 1
-    total_num = index
   else
     result = result + string[i]
-    result = result + total_num.to_s if total_num > 1
+    result = result + index.to_s if index > 1
     index = 1
-    total_num = 1
   end
 i += 1
 end
