ab594f4428a6478852674197c5ad16e647806a3c
skibiska <lskibitska@gmail.com>
2016-10-04 23:22:34 +0300

    fixed again hw2

--- hw2/string hw.rb
+++ hw2/string hw.rb
@@ -1,23 +1,24 @@
 puts "Enter some string"
 string = gets
-l = string.length
+s_length = string.length
 result = ''
 index = 1
- for i in (0..l-2) do
- 	if string[i] == string[i+1]
- 		index +=1
- 		k = index
- 	else
- 		result = result + k.to_s if k > 1
- 		result = result + string[i]
- 		index = 1
-  		k = 1
- 	end
-		i +=1
- end
+
+for i in (0..s_length-2) do
+  if string[i] == string[i+1]
+    index += 1
+    total_num = index
+  else
+    result = result + string[i]
+    result = result + total_num.to_s if total_num > 1
+    index = 1
+    total_num = 1
+  end
+i += 1
+end
  	
- result = result + string[l-1]
- puts"Result = #{result}"
+result = result + string[s_length-1]
+puts "Result = #{result}"
 
 
 
