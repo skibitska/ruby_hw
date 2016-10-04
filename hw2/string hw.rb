9bd5b5e602d5099ec8858cc6c906a9d62b8ecd29
skibiska <lskibitska@gmail.com>
2016-10-04 12:13:51 +0300

    added hw2

---
+++ hw2/string hw.rb
@@ -0,0 +1,25 @@
+puts "Enter some string"
+string = gets
+l = string.length
+result = ''
+index = 1
+ for i in (0..l-2) do
+ 	if string[i] == string[i+1]
+ 		index +=1
+ 		k = index
+ 	else
+ 		result = result + k.to_s + string[i]
+ 		index = 1
+  		k = 1
+ 	end
+		i +=1
+ end
+ 	
+ result = result + string[l-1]
+ puts"Result = #{result}"
+
+
+
+
+
+ 	
